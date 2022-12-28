893e642f5aa6d3d4a980b633e44b2840d0fa019f
Anshuman202 <asanshuman2@gmail.com>
2022-12-28 09:10:31 +0530

    adding Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeWageProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welocome to Employee Wage Problem");
+
+            EmployeeAttendence.CheckAttendence();
+        }
+    }
+}
