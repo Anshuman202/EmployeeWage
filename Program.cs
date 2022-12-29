448e996c4186c8e099854d9c0f808f107246e399
Anshuman202 <119556991+Anshuman202@users.noreply.github.com>
2022-12-29 15:30:47 +0530

    modified

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welocome to Employee Wage Problem");
 
-            EmployeeAttendence.CheckAttendence();
+            EmployeeLimitedWageCalculate.Cal_limitedwage();
         }
     }
 }
